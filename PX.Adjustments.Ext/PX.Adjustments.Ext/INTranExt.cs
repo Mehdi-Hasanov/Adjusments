38e3506643897b34a25c972674ce3c946ebb5a69
Mehdi-Hasanov <119957183+Mehdi-Hasanov@users.noreply.github.com>
2023-07-24 14:32:30 -0400

    fixed

---
+++ PX.Adjustments.Ext/PX.Adjustments.Ext/INTranExt.cs
@@ -0,0 +1,40 @@
+using PX.Data;
+using System;
+
+namespace PX.Objects.IN
+{
+    public class INTranExt : PXCacheExtension<PX.Objects.IN.INTran>
+    {
+        #region UsrCurrentQtyOnHand
+        public abstract class usrCurrentQtyOnHand : PX.Data.BQL.BqlDecimal.Field<usrCurrentQtyOnHand> { }
+
+        [PXDBQuantity]
+        [PXUIField(DisplayName = "Current Qty. On Hand")]
+        public virtual Decimal? UsrCurrentQtyOnHand { get; set; }
+        #endregion
+
+        #region UsrAdjustedQtyOnHand
+        public abstract class usrAdjustedQtyOnHand : PX.Data.BQL.BqlDecimal.Field<usrAdjustedQtyOnHand> { }
+
+        [PXQuantity]
+        [PXUIField(DisplayName = "Adjusted Qty. On Hand")]
+        public virtual Decimal? UsrAdjustedQtyOnHand { get; set; }
+        #endregion
+
+        #region UsrCurrentTranCost
+        public abstract class usrCurrentTranCost : PX.Data.BQL.BqlDecimal.Field<usrCurrentTranCost> { }
+
+        [PXDBDecimal]
+        [PXUIField(DisplayName = "Current Ext. Cost")]
+        public virtual Decimal? UsrCurrentTranCost { get; set; }
+        #endregion
+
+        #region UsrAdjustedTranCost
+        public abstract class usrAdjustedTranCost : PX.Data.BQL.BqlDecimal.Field<usrAdjustedTranCost> { }
+
+        [PXDecimal]
+        [PXUIField(DisplayName = "Adjusted Ext. Cost")]
+        public virtual Decimal? UsrAdjustedTranCost { get; set; }
+        #endregion
+    }
+}
